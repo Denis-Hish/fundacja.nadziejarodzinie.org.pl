3d3ac27836bfa6dce3bb1c4e466e8405d7996c55
Denis-Hish <denis.hish@gmail.com>
2023-04-11 15:08:59 +0200

    Мелкие правки

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,12 +19,40 @@ for (var i = 0; i < links.length; i++) {
 }
 
 // положение верхней части экрана
-const counterElement = document.querySelector('.counter');
-function updateCounter() {
-   counterElement.innerHTML = Math.round(window.scrollY) + ' px';
-}
-updateCounter();
-window.addEventListener('scroll', updateCounter);
+// const counterElement = document.querySelector('.counter');
+// function updateCounter() {
+//    counterElement.innerHTML = Math.round(window.scrollY) + ' px';
+// }
+// updateCounter();
+// window.addEventListener('scroll', updateCounter);
+
+//-------------------------FIXED-MENU------------------------------//
+
+window.addEventListener('scroll', function () {
+   const mainHeader = document.querySelector('.main-header');
+   const listMenu = document.querySelector('.desktop-nav');
+   const logoBig = document.querySelector('.logo-big');
+   const logoSmall = document.querySelector('.logo-small');
+   const menuIcon = document.querySelector('.menu-icon');
+   const mobileNav = document.querySelector('.mobile-nav');
+   const scrollPosition = window.pageYOffset;
+
+   if (scrollPosition >= 100) {
+      mainHeader.classList.add('main-header-fixed');
+      listMenu.classList.add('list-fixed');
+      menuIcon.classList.add('menu-icon-fixed');
+      mobileNav.classList.add('mobile-nav-fixed');
+      logoBig.style.opacity = '0';
+      logoSmall.style.opacity = '1';
+   } else {
+      mainHeader.classList.remove('main-header-fixed');
+      listMenu.classList.remove('list-fixed');
+      menuIcon.classList.remove('menu-icon-fixed');
+      mobileNav.classList.remove('mobile-nav-fixed');
+      logoBig.style.opacity = '1';
+      logoSmall.style.opacity = '0';
+   }
+});
 
 //------------------------ menu & logo ----------------------------//
 // function onScroll() {
